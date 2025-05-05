27bae692ce05aaac0609a69f27279849bd0d6eb1
VoHoangKiet <muoivo.051064@gmail.com>
2025-05-05 13:33:04 +0700

    add pipeline

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { ValidationExceptionFilter } from './common/filters/validation-exception.filter';
+import { EnvironmentVariable } from './common/enums/enviroment.enum';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const service = app.get(ConfigService);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new ValidationExceptionFilter());
+  app.setGlobalPrefix(service.getOrThrow(EnvironmentVariable.CONTEXT_PATH));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
